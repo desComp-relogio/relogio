4b49242abcd4bcc54c7ad70a1c0312e5133a7c1d
Raphael <rapha2706@hotmail.com>
2018-09-24 16:15:14 -0700

    ajuste pronto

--- relogio.vhd
+++ relogio.vhd
@@ -28,7 +28,7 @@ architecture comportamento of relogio is
 	signal Mux_sel_6_aux: std_logic_vector(2 downto 0) := "000";
 	signal Mux_sel_5_aux: std_logic_vector(2 downto 0) := "000";
 	signal funcaoULA_aux: std_logic_vector(1 downto 0) := "00";
-	signal state_aux	  : std_logic_vector(3 downto 0) := "0000";
+	signal state_aux	  : std_logic_vector(4 downto 0) := "00000";
 	signal is_ajusta_aux: std_logic := '0';
 	signal auxReset : std_logic := '0';
 	signal valor_ajusta_aux:  std_logic_vector(3 downto 0);		 
@@ -47,7 +47,13 @@ architecture comportamento of relogio is
 	signal ResUS_aux:  std_logic := '0';
 	signal sempre	 :	 std_logic := '1';
 	signal um_seg	 :	 std_logic := '0';
-	
+	signal ajusta	 :	 std_logic := '0';
+	signal but_doneUM :  std_logic := '0';
+	signal but_doneDM :  std_logic := '0';
+	signal but_doneUH :  std_logic := '0';
+	signal but_doneDH :  std_logic := '0';
+	signal sw_value : std_logic_vector(3 downto 0) := "0000";
+
 	signal Z_aux	  : std_logic;
 	signal DH_out_aux: std_logic_vector(3 downto 0);
 	signal UH_out_aux: std_logic_vector(3 downto 0);
@@ -101,15 +107,16 @@ begin
 --  --  2 = XOR
 --  --  3 = AND
   display6 : entity work.conversorHex7seg
-    Port map (saida7seg => HEX6, dadoHex => '0' &  '0' & '0' & um_seg);
+    Port map (saida7seg => HEX6, dadoHex => valor_ajusta_aux);
 --
   -- Indica o estado atual da maquina de estado, em decimal:
   display7 : entity work.conversorHex7seg
-    Port map (saida7seg => HEX7, dadoHex => '0' &  '0' & '0' & Z_aux);
+    Port map (saida7seg => HEX7, dadoHex => '0' &  '0' & '0' & SW(17));
 --
   -- Instacia a maquina de estados:
 	sequenciador : entity work.SM1
     port map( reset => auxReset, clock => CLOCK_50,
+				  ajusta => ajusta, but_doneUM => but_doneUM, but_doneDM => but_doneDM, but_doneUH => but_doneUH, but_doneDH => but_doneDH, 
 				  Z => Z_aux, sempre => sempre, um_seg => um_seg,controle => controle, state => state_aux);  --,
 				  
 	clk_1seg: entity work.clk_div port map(clk_50 => CLOCK_50, clk_1s => um_seg);
@@ -133,30 +140,53 @@ begin
 	Mux_sel_5_aux <= controle(5 downto 3);
 	Mux_sel_6_aux <= controle(2 downto 0);
 	
---	Mux_sel_6 <= mux_sel_6_aux;
---	Mux_sel_5 <= mux_sel_5_aux;
---	funcaoULA <= funcaoULA_aux;
---	state <= state_aux;
---	is_ajusta <= is_ajusta_aux;
---	EnDH <= EnDH_aux;
---	EnUH <= EnUH_aux;
---	EnDM <= EnDM_aux;
---	EnUM <= EnUM_aux;
---	EnDS <= EnDS_aux;
---	EnUS <= EnUS_aux;
---	ResDH <= ResDH_aux;
---	ResUH <= ResUH_aux;
---	ResDM <= ResDM_aux;
---	ResUM <= ResUM_aux;
---	ResDS <= ResDS_aux;
---	ResUS <= ResUS_aux;
---	DH_out <= DH_out_aux;
---	UH_out <= UH_out_aux;
---	DM_out <= DM_out_aux;
---	UM_out <= UM_out_aux;
---	DS_out <= DS_out_aux;
---	US_out <= US_out_aux;
---	controle_out <= controle;
---	um_seg_out <= um_seg;
+	ajusta <= SW(17);
+	
+	but_doneUM <= KEY(0);
+	but_doneDM <= KEY(1);
+	but_doneUH <= KEY(2);
+	but_doneDH <= KEY(3);
+	
+	sw_value <= SW(3 downto 0);
+	
+	process(sw_value, state_aux)
+	begin
+	if to_integer(unsigned(state_aux)) = 17 then
+	
+		case to_integer(unsigned(sw_value)) 	is
+			when 10 to 15 => valor_ajusta_aux <= "1001";
+			when others => valor_ajusta_aux <= sw_value;
+		end case;
+	
+	elsif to_integer(unsigned(state_aux)) = 18 then
+		
+		case to_integer(unsigned(sw_value)) 	is
+			when 6 to 15 => valor_ajusta_aux <= "0101";
+			when others => valor_ajusta_aux <= sw_value;
+		end case;
+	
+	elsif to_integer(unsigned(state_aux)) = 19 then
+		
+		if ( DH_out_aux = "0010") then 
+			case to_integer(unsigned(sw_value)) 	is
+				when 4 to 15 => valor_ajusta_aux <= "0011";
+				when others => valor_ajusta_aux <= sw_value;
+			end case;
+		else
+			case to_integer(unsigned(sw_value)) 	is
+				when 10 to 15 => valor_ajusta_aux <= "1001";
+				when others => valor_ajusta_aux <= sw_value;
+			end case;
+		end if;
+		
+	elsif to_integer(unsigned(state_aux)) = 20 then
+		
+		case to_integer(unsigned(sw_value)) 	is
+			when 3 to 15 => valor_ajusta_aux <= "0010";
+			when others => valor_ajusta_aux <= sw_value;
+		end case;
+	
+	end if;
+	end process;
 
 end architecture;
